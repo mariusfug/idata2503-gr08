3652c73517a577f9bd3eefc78e677b46ccc402f1
mariufug <77608175+mariusfug@users.noreply.github.com>
2022-09-22 15:16:02 +0200

    Refactored home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:idata2503_group08/home_page.dart';
+import 'package:idata2503_group08/main_pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
