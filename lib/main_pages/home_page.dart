5f73d9f2c7458705bf82f64b1237ff40ba109061
mariufug <77608175+mariusfug@users.noreply.github.com>
2022-09-29 12:38:03 +0200

    Made reusable bottom navigation bar

--- lib/main_pages/home_page.dart
+++ lib/main_pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:idata2503_group08/navigation/bottom_navigation.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,36 +13,7 @@ class HomePage extends StatelessWidget {
         title: const Text('Forum app'),
         backgroundColor: const Color(0xFF1E1E1E),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: const Color(0xFF1E1E1E),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: const Color(0x90B8B8B8),
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home, size: 30),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.explore, size: 30),
-              label: 'Discover',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add, size: 45),
-              label: 'New post',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.group, size: 30),
-              label: 'Groups',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat, size: 30),
-              label: 'Chats',
-            ),
-          ]),
+      bottomNavigationBar: const BottomNavigation(),
     );
   }
 }
