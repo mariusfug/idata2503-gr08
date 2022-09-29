548ea252942d3fe01b7d4359799d9b98da7ae1d0
Mr.Frank <Mr.Frank@DESKTOP-JDALJDU.local>
2022-09-29 12:38:40 +0200

    Update main.dart and login_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:idata2503_group08/main_pages/home_page.dart';
 
+import 'main_pages/login_page.dart';
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
