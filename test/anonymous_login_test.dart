d4796b459080ef1885fa2bc1ecd8bfca8ebe355a
DESKTOP-6PA949H\tgikl <torog@stud.ntnu.no>
2022-11-08 20:52:25 +0100

    Working on some testing and added some keys

---
+++ test/anonymous_login_test.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:idata2503_group08/app/sign_in/login_page.dart';
+import 'package:idata2503_group08/services/auth.dart';
+
+void main()  {
+  // TODO: Make something that can be tested
+
+}
