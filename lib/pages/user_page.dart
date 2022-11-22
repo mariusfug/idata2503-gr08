80b4e18121ad6b1de2832129fc5892e3b1df55ff
Tor Gikling <torog@stud.ntnu.no>
2022-11-22 16:36:31 +0100

    Created user page. Almost finished just needs the different text buttons to work

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:idata2503_group08/services/auth.dart';
 import 'package:idata2503_group08/widgets/navigation/bottom_navigation.dart';
 import 'package:idata2503_group08/widgets/navigation/top_navigation.dart';
 
@@ -15,36 +17,79 @@ class _UserPage extends State<UserPage> {
     return Scaffold(
       backgroundColor: const Color(0xFF1E1E1E),
       appBar: TopNavigation(),
-      body: Container(
+      body: Center(
         child: SingleChildScrollView(
+          padding: const EdgeInsets.all(10),
           child: Column(
             children: [
-              const SizedBox(
-                height: 100,
+              RawMaterialButton(
+                onPressed: () {},
+                elevation: 2.0,
+                fillColor: const Color(0xFFFFFFFF),
+                shape: const CircleBorder(),
+                child: const ImageIcon(
+                  AssetImage("resources/icons/user_outline.png"),
+                  size: 200,
+                ),
               ),
-              Row(
-                children: [
-                  const Text("UserProfile"),
-                  IconButton(
-                    icon: const Icon(Icons.arrow_downward),
-                    onPressed: () {
-                      print("button is pressed");
-                    },
+
+              const SizedBox(height: 16),
+
+              const TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  border: OutlineInputBorder(),
+                  fillColor: Color(0xffB8B8B8),
+                  labelText: "Write BIO here",
+
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                    "Settings",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 24,
                   ),
-                  const SizedBox(
-                    height: 30,
+                ),
+              ),
+
+              const SizedBox(height: 16.0),
+
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Terms of Service",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 24,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("logout button pressed");
-                    },
-                    child: const Text(
-                      "Logout",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+
+              const SizedBox(height: 16.0),
+
+              TextButton(
+                  onPressed: () => {
+                    _signOut(context),
+                  },
+                  child: const Text(
+                    "Sign out",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontSize: 24
                     ),
-                  )
-                ],
-              )
+                  ),
+              ),
+
+
             ],
           ),
         ),
@@ -52,5 +97,15 @@ class _UserPage extends State<UserPage> {
       ),
       bottomNavigationBar: const BottomNavBar(),
     );
+
+
+  }
+  Future<void> _signOut(BuildContext context) async {
+    try {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    await auth.signOut();
+    } catch (e) {
+    print(e.toString());
+    }
   }
 }
