685983faf1a4b83c7308ba2c9239793869fdecba
Mr.Frank <Mr.Frank@DESKTOP-JDALJDU.local>
2022-11-23 15:04:30 +0100

    Started the basic skeleton for the group page. still not finished need to find a way to fit 2 elements in a row

--- lib/pages/groups_page.dart
+++ lib/pages/groups_page.dart
@@ -6,9 +6,48 @@ class GroupsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color(0xFF1E1E1E),
       appBar: TopNavigation(),
       bottomNavigationBar: const BottomNavBar(),
-    );
+      body:SingleChildScrollView(
+        child:
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+              children: [
+                    RawMaterialButton(onPressed: () { },
+                        elevation: 2.0,
+                        fillColor: const Color(0xFFFFFFFF),
+                        shape: const CircleBorder(),
+                        child: const ImageIcon(
+                          AssetImage("resources/icons/user_outline.png"),
+                          size: 180,
+                        )),
+                    RawMaterialButton(onPressed: () { },
+                        elevation: 2.0,
+                        fillColor: const Color(0xFFFFFFFF),
+                        shape: const CircleBorder(),
+                        child: const ImageIcon(
+                          AssetImage("resources/icons/user_outline.png"),
+                          size: 180,
+                        )),
+                    RawMaterialButton(onPressed: () { },
+                        elevation: 2.0,
+                        fillColor: const Color(0xFFFFFFFF),
+                        shape: const CircleBorder(),
+                        child: const ImageIcon(
+                          AssetImage("resources/icons/user_outline.png"),
+                          size: 180,
+                        ))
+                  ],
+                )
+
+            )
+
+
+      );
+
   }
 
 }
