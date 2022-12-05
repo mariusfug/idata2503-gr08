803bbfbe3db0d08393caa8beb8395ad53c61e526
Mr.Frank <Mr.Frank@DESKTOP-JDALJDU.local>
2022-12-05 14:35:59 +0100

    Functionality to add groups

--- lib/model/group.dart
+++ lib/model/group.dart
@@ -1,23 +1,19 @@
-
 class Group {
-  int id;
+  String id;
   String name;
 
   Group({
     required this.id,
     required this.name,
-});
+  });
 
   static Group fromMap(Map<String, dynamic> data) {
     assert(data.containsKey("id"), "Missing id property");
     assert(data.containsKey("name"), "Missing name property");
 
     return Group(
-      id: data["id"],
+      id: data["id"].toString(),
       name: data["name"],
     );
   }
-
-
-
 }
